1425e73389ff0ccff3a5e06c933e3f13e7d7e697
Lado <Tsivtsivadzelado@yahoo.com>
2021-10-07 06:01:48 -0700

    Initial Commit

---
+++ WeeklyCalendar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeeklyCalendar
+//
+//  Created by Ladolado3911 on 07.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
