e0521bbf6848bf9456103084e2c66688900da9a6
Lado <Tsivtsivadzelado@yahoo.com>
2021-10-08 02:35:06 -0700

    added button and dayviewcontroller

---
+++ WeeklyCalendar/Presentation/Scenes/CalendarScene/CalendarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarViewController.swift
+//  WeeklyCalendar
+//
+//  Created by Ladolado3911 on 08.10.21.
+//
+
+import UIKit
+import CalendarKit
+
+class CalendarViewController: DayViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ WeeklyCalendar/Presentation/Scenes/MainScene/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  WeeklyCalendar
+//
+//  Created by Ladolado3911 on 07.10.21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onOpenCalendar(_ sender: Any) {
+        let sb = UIStoryboard(name: "Calendar", bundle: nil)
+        let vc = sb.instantiateViewController(withIdentifier: "CalendarViewController")
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
